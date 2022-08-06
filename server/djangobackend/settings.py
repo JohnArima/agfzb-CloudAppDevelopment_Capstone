3b88e1a6679331324d074802dce13c1a6181ff3e
JohnArima <john.salangsang20@gmail.com>
2022-08-06 14:00:52 +0000

    Adding temporary changes to Github

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["localhost", "john-capstoneproject-djangoapp.us-south.mybluemix.net"]
 
 
 # Application definition
